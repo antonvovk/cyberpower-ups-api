c338666bc211bf3adbeabbee27c44fb41d094853
Anton Vovk <archwolf@protonmail.com>
2024-06-23 17:44:06 +0300

    Initial commit

---
+++ src/main/java/com/antonvovk/cyberpowerupsapi/CyberpowerUpsApiApplication.java
@@ -0,0 +1,13 @@
+package com.antonvovk.cyberpowerupsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberpowerUpsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberpowerUpsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/antonvovk/cyberpowerupsapi/CyberpowerUpsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonvovk.cyberpowerupsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberpowerUpsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
